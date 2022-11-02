fc1c5fd4fb9ceedf5f59640dac6ee6ce73fe4f20
Matheus <kroskamatheus@gmail.com>
2022-11-02 07:25:39 -0300

    refactor(usebatchweather): improved performance changing function to resolve promises once

--- src/commom/useBatchWeather.js
+++ src/commom/useBatchWeather.js
@@ -1,27 +1,58 @@
 import { getCity, getWeather } from '../services/api'
 import { addWeather } from '../services/database'
 
+/**
+ * It takes a location as an argument, and returns a promise that resolves to an array of random
+ * cities.
+ * @param location - The location to search for.
+ */
 const randomCities = async (location) => {
   try {
-    const cities = await getCity(location)
-
-    const randomCities = cities.options.sort(() => 0.5 - Math.random())
-    const randomCities3 = randomCities.slice(0, 1)
-
-    randomCities3.forEach(async (city) => {
-      const cityWeather = await getWeather(city)
-      await addWeather(cityWeather)
+    let promiseCity = []
+    location.map(async (city) => {
+      promiseCity.push(getCity(city))
     })
+    const cities = await Promise.all(promiseCity)
+
+    let randomCities = []
+    cities.map(async (city) => {
+      randomCities.push(
+        city.options[Math.floor(Math.random() * city.options.length)],
+      )
+    })
+
+    let promiseWeather = []
+    randomCities.map(async (city) => {
+      promiseWeather.push(getWeather(city))
+    })
+    const weather = await Promise.all(promiseWeather)
+
+    let promiseAddWeather = []
+    weather.map(async (item) => {
+      promiseAddWeather.push(addWeather(item))
+    })
+    await Promise.all(promiseAddWeather)
+
+    return weather
   } catch (error) {
-    console.error(error)
+    throw error
   }
 }
 
 export const useBatchWeather = () => {
-  randomCities('Brazil')
-  randomCities('United States')
-  randomCities('Japan')
-  randomCities('Germany')
-  randomCities('France')
-  randomCities('Argentina')
+  randomCities([
+    'Brazil',
+    'Germany',
+    'France',
+    'Spain',
+    'Argentina',
+    'Italy',
+    'England',
+    'Portugal',
+    'USA',
+    'Canada',
+    'China',
+    'Japan',
+    'Russia',
+  ])
 }
